da49085cbad303643625e522ed2195383e0295d3
shituweian <1258264684@qq.com>
2020-05-24 11:55:15 +0800

    更新了换关机制

--- src/main/java/gamesource/start.java
+++ src/main/java/gamesource/start.java
@@ -4,7 +4,7 @@ import com.jme3.system.AppSettings;
 
 public class start {
     public  void Start(int width,int length,int frame,int state){
-        App app=new App(1);
+        App app=new App(state);
         AppSettings settings =new AppSettings(true);
         settings.setTitle("malware");
         settings.setResolution(width,length);
