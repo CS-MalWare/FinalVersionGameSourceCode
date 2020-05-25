709c9b1cfb8765aea9c549e62502affffcb6be22
“lkczn <919535598@qq.com>
2020-05-25 19:21:27 +0800

    第五世界boss调好，但打赢后有bug

--- src/main/java/gamesource/battleState/appState/EnemyState.java
+++ src/main/java/gamesource/battleState/appState/EnemyState.java
@@ -404,6 +404,10 @@ public class EnemyState extends BaseAppState {
             case "Enemies/fifthMap/airplane2.j3o":
                 model.scale(0.07f);
                 break;
+            case "Enemies/fifthMap/boss/scene0.j3o":
+                model.scale(0.013f);
+                model.rotate(0,3.7f,0);
+                break;
             default:
                 break;
         }
@@ -411,6 +415,9 @@ public class EnemyState extends BaseAppState {
 
     private void setModelPos(Spatial model) {
         switch (model.getName()) {
+            case "Enemies/fifthMap/boss/scene0.j3o":
+                model.move(0.5f, 0.1f, 0);
+                break;
             case "Enemies/fifthMap/airplane0.j3o":
                 model.move(-1f, 0.2f, 0);
                 break;
@@ -932,6 +939,7 @@ public class EnemyState extends BaseAppState {
                     case "Enemies/fifthMap/mech_drone/scene0.j3o":
                     case "Enemies/fifthMap/mech_drone/scene1.j3o":
                     case "Enemies/fifthMap/mech_drone/scene2.j3o":
+                    case "Enemies/fifthMap/boss/scene.j3o":
                         deadSoundSrc = "Sound/Dead/机器人死亡.wav";
                         break;
 //                    case "3":
