65d7b514ed9d4a6acb6d1cae7a26e00675db0c60
TianHanYueDeng <944389333@qq.com>
2020-05-25 15:33:02 +0800

    第三个世界宝箱逻辑加上

--- src/main/java/gamesource/State/worldState/ThirdState.java
+++ src/main/java/gamesource/State/worldState/ThirdState.java
@@ -312,12 +312,18 @@ public class ThirdState extends BaseAppState {
                 } else if (results7 != null && results7.size() > 0) {
                     System.out.println("chest");
                     c1.open();
+                    getApplication().getStateManager().attach(new GetCardState());
+
                 } else if (results8 != null && results8.size() > 0) {
                     System.out.println("chest");
                     c2.open();
+                    getApplication().getStateManager().attach(new GetCardState());
+
                 } else if (results9 != null && results9.size() > 0) {
                     System.out.println("chest");
                     c3.open();
+                    getApplication().getStateManager().attach(new GetCardState());
+
                 }
             }
 
