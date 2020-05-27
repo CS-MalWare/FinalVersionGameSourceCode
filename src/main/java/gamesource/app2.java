bd931961e9dad0aa72606a6e96679756ca384b55
shituweian <1258264684@qq.com>
2020-05-27 08:40:43 +0800

    修复

---
+++ src/main/java/gamesource/app2.java
@@ -0,0 +1,8 @@
+package gamesource;
+
+public class app2 {
+    public static void main(String[] args){
+        start start1=new start();
+        start1.Start(1,1,1,1,1,true);
+    }
+}
--- src/main/java/gamesource/start.java
+++ src/main/java/gamesource/start.java
@@ -5,7 +5,7 @@ import gamesource.util.Storage;
 
 public class start {
     public  void Start(int frame,int state,int shadow,int open,int juchi,boolean full){
-        App app = new App(state, shadow, open, juchi);
+        App app = new App(6, 1024, 1, 4);
         AppSettings settings = new AppSettings(true);
 
         // 这里是导入地图存档的,导入人物属性存档在 MainRole类中
@@ -13,12 +13,31 @@ public class start {
 
         settings.setTitle("Cholera");// 标题
         settings.setResolution(1600, 900);// 分辨率
-        settings.setFrameRate(frame);//限制fps
+        settings.setFrameRate(100);//限制fps
 //        settings.setIcons(new BufferedImage[]{
 //                ImageIO.read(Main.class.getResource( "/Map/icon.png" ))});
         app.setSettings(settings);
+        settings.setFullscreen(true);
         app.setShowSettings(false);
-        settings.setFullscreen(full);
+        //app.setShowSettings(false);
+//        Storage.load();
+        app.start();
+    }
+    public  static void main(String[] args){
+        App app = new App(6, 1024, 1, 4);
+        AppSettings settings = new AppSettings(true);
+
+        // 这里是导入地图存档的,导入人物属性存档在 MainRole类中
+        //Storage.load();
+
+        settings.setTitle("Cholera");// 标题
+        settings.setResolution(1600, 900);// 分辨率
+        settings.setFrameRate(100);//限制fps
+//        settings.setIcons(new BufferedImage[]{
+//                ImageIO.read(Main.class.getResource( "/Map/icon.png" ))});
+        app.setSettings(settings);
+        settings.setFullscreen(true);
+        app.setShowSettings(false);
         //app.setShowSettings(false);
 //        Storage.load();
         app.start();
